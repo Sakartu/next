30afc94192cc613316b1d33d5afd545acb997edb
Peter Wagenaar <sakartu@gmail.com>
2011-10-18 15:31:34 +0200

    added find_show

--- show/show.py
+++ show/show.py
@@ -1,2 +1,7 @@
-def play_next(show_name):
+def find_show(db_conn, show_name):
+    c = db_conn.cursor()
+    c.execute('SELECT * FROM shows WHERE name=?', show_name)
+    pass
+
+def play_next(show):
     pass
