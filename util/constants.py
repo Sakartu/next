6ad21e871c8f375b221cf343a2298789f36fdc77
Peter Wagenaar <sakartu@gmail.com>
2011-10-18 14:32:14 +0200

    added some constants

---
+++ util/constants.py
@@ -0,0 +1,12 @@
+CONF_PATH = '~/.next/next.conf'
+VIDEO_EXTS = ['avi', 'mpg', 'mpeg', 'mkv']
+SHOW_REGEXES = [r'.*S(\d{1,2)E(\d{1,2}).*']
+
+USAGE = \
+        """Usage: %prog [options] [show]
+
+This program helps you in maintaining your episodes. It
+will remember which eps you've already seen and start a
+new named (or a new random) ep for you. The configuration
+file for %prog can be found in ~/.%prog/
+"""
