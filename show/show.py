4807fb5dbab9c75940538ef9a126367014dc67d4
Peter Wagenaar <sakartu@gmail.com>
2011-10-20 23:06:08 +0200

    made sure show seasons and eps are ints

--- show/show.py
+++ show/show.py
@@ -1,5 +1,5 @@
 class Show:
     def __init__(self, db_row):
         self.name = db_row[0]
-        self.season = db_row[1]
-        self.ep = db_row[2]
+        self.season = int(db_row[1])
+        self.ep = int(db_row[2])
