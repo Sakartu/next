7ebecaa1abad65db13227c3e4ac3a09cfcf29d7e
Peter Wagenaar <sakartu@gmail.com>
2011-10-21 22:09:12 +0200

    beautified some stuff

--- util/constants.py
+++ util/constants.py
@@ -1,17 +1,17 @@
-import re
-
 CONF_PATH = '~/.next/next.conf'
 VIDEO_EXTS = ['.avi', '.mpg', '.mpeg', '.mkv'] #need to have .'s in front!
-SHOW_REGEXES = [r'.*S0*(?P<season>{season})E0*(?P<ep>{ep}).*', #accounts for most S12E12 type of shows
-                r'.*0*(?P<season>{season})x0*(?P<ep>{ep}).*'] #accounts for most 12x12 type of shows
 
-USAGE = \
-        """Usage: %prog [options] [show]
+SHOW_REGEXES = [
+    r'.*S0*(?P<season>{season})E0*(?P<ep>{ep}).*',  #accounts for most S12E12 type of shows
+    r'.*0*(?P<season>{season})x0*(?P<ep>{ep}).*'    #accounts for most 12x12 type of shows
+    ] 
+
+USAGE = """Usage: next [options] [show]
 
 This program helps you in maintaining your episodes. It
 will remember which eps you've already seen and start a
 new named (or a new random) ep for you. The configuration
-file for %prog can be found in ~/.%prog/
+file for nextcan be found in ~/.next/
 """
 
 class Keys:
