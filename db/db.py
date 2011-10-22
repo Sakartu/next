3d29c0b2f9e6e2b8414305f08577fadb07114e55
Peter Wagenaar <sakartu@gmail.com>
2011-10-22 14:08:55 +0200

    beautified some of the code in db.py and added another table for tvr

--- db/db.py
+++ db/db.py
@@ -12,13 +12,15 @@ def initialize(path):
                 WHERE type='table'
                 AND name='shows'""").fetchall()
         if not test:
-            c.execute("""CREATE TABLE shows(name text, season integer, ep integer)""")
+            c.execute("""CREATE TABLE shows(name text, season integer, ep
+                    integer)""")
+            c.execute("""CREATE TABLE tvr_shows(name text, season integer, ep
+                    integer, airdate text)""")
         
     return conn
 
 def find_show(conf, show_name):
-    conn = conf[Keys.DB_CONN]
-    with conn:
+    with conf[Keys.DB_CONN] as conn:
         c = conn.cursor()
         c.execute("""SELECT * FROM shows 
                 WHERE name like ?""", ("%" + show_name + "%",))
@@ -33,29 +35,33 @@ def find_show(conf, show_name):
     return Show(shows[0])
 
 def add_show(conf, showname, season, ep):
-    conn = conf[Keys.DB_CONN]
-    with conn:
+    with conf[Keys.DB_CONN] as conn:
         c = conn.cursor()
         c.execute('INSERT INTO shows VALUES (?, ?, ?)', (showname, season, ep,))
 
 def change_show(conf, showname, season, ep):
-    conn = conf[Keys.DB_CONN]
-    with conn:
+    with conf[Keys.DB_CONN] as conn:
         c = conn.cursor()
         c.execute("""UPDATE shows SET season=?, ep=? where name like ?""", (season, ep, "%" + showname + "%",))
 
 def all_shows(conf):
-    conn = conf[Keys.DB_CONN]
-    with conn:
+    with conf[Keys.DB_CONN] as conn:
         c = conn.cursor()
         c.execute("""SELECT * FROM shows""")
         shows = c.fetchall()
         return map(Show, shows)
 
 def find_next_ep(conf, showname):
-    conn = conf[Keys.DB_CONN]
-    with conn:
+    with conf[Keys.DB_CONN] as conn:
         c = conn.cursor()
         c.execute("""SELECT season, ep FROM shows where name like ?""", "%" + showname + "%")
         print c.fetchall()
         return map(lambda x : x[0], c.fetchall())
+
+def store_tvr_eps(conf, show, eps):
+    with conf[Keys.DB_CONN] as conn:
+        c = conn.cursor()
+
+
+
+
