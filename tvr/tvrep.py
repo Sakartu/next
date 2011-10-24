983431184323f8533ee5eb49ecb94a7302c4101d
Peter Wagenaar <sakartu@gmail.com>
2011-10-24 13:52:24 +0200

    added docstrings to tvrepisode

--- tvr/tvrep.py
+++ tvr/tvrep.py
@@ -1,4 +1,8 @@
 class Episode:
+    '''
+    This is a wrapper class around the episode information available in the
+    TVRage database for a given episode.
+    '''
     def __init__(self, sid, showname, season, epnum, title, airdate):
         self.sid = sid
         self.showname = showname
@@ -9,6 +13,10 @@ class Episode:
 
     @staticmethod
     def from_db_row(row):
+        '''
+        This static method can be used to create an Episode object from a
+        database row
+        '''
         return Episode(row[0], row[1], row[2], row[3], row[4], row[5])
 
     def __str__(self):
