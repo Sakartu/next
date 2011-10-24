6b369e76ff9c2dc93ad07895a934960b1d58294e
Peter Wagenaar <sakartu@gmail.com>
2011-10-24 13:46:46 +0200

    added docstrings for all the show/admin.py functions

--- show/admin.py
+++ show/admin.py
@@ -3,12 +3,19 @@ from tvr import parser
 import os
 
 def find_unlisted(conf):
+    '''
+    This method searches in your shows folder for shows that aren't in the
+    database yet
+    '''
     listed = map(lambda x : x.name, db.all_shows(conf))
     basedir = os.path.expanduser(conf['show_path'])
     all_shows = filter(lambda x : os.path.isdir(os.path.join(basedir, x)), os.listdir(basedir))
     return list(set(all_shows) - set(listed))
 
 def find_next_ep(conf, show):
+    '''
+    This method tries to determine which ep is next for the given show.
+    '''
     next_season = show.season
     next_ep = show.ep + 1
     ep = db.find_ep(conf, show.sid, next_season, next_ep)
@@ -19,6 +26,9 @@ def find_next_ep(conf, show):
     return ep
 
 def update_eps(conf):
+    '''
+    This method updates the eplist for a given show using the TVRage database
+    '''
     #first we check tvr to see if there are any updates for our shows
     all_shows = db.all_shows(conf)
     try:
@@ -32,16 +42,16 @@ def update_eps(conf):
     process_maybe_finished(conf, all_shows)
 
 def process_maybe_finished(conf, all_shows):
+    '''
+    This method walks over all the shows that are marked maybe_finished, to find
+    out if they're really finished or if the database was just outdated. If a
+    new ep can be found for the given show, this is set as the current ep and
+    the maybe_finished mark is removed
+    '''
     for show in all_shows:
         if show.maybe_finished:
             next_ep = find_next_ep(show)
             if next_ep:
                 db.change_show(conf, show.sid, next_ep.season, next_ep.epnum)
                 db.mark_not_maybe_finished(conf, show.sid)
-
-    
-
-
-    
-
 
