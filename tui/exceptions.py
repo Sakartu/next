33b4c15bed50a44747ad1bfe5ea40a18d7056f72
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2011-10-25 12:13:15 +0200

    Include functions in the TUI class

--- tui/exceptions.py
+++ tui/exceptions.py
@@ -1,5 +1,17 @@
-class UserCancelled(Exception):
+class TUIException(Exception):
+    '''
+    A TUI exception
+    '''
+    pass
+
+class UserCancelled(TUIException):
     '''
     This class is raised when the user presses ^D in the TUI
+    '''
+    pass
+
+class NoShowsException(TUIException):
+    '''
+    Class raides when there are no shows in the database
     '''
     pass
