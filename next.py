43b661791b44afb337c035f523b98c9f915e90dd
Peter Wagenaar <sakartu@gmail.com>
2011-10-28 21:11:36 +0200

    some beautification

--- next.py
+++ next.py
@@ -19,7 +19,7 @@ except:
 def main():
     (conf, args) = config.parse_opts()
 
-    try: #the database_path is usually the show_path, but can be defined in conf
+    try: # the database_path is usually the show_path, but can be defined in conf
         if ConfKeys.DB_PATH in conf:
             database_path = os.path.join(conf[ConfKeys.DB_PATH], u'.next.db')
         else:
@@ -29,7 +29,9 @@ def main():
         print(u'No show_path or database_path defined in configuration, aborting!')
         sys.exit(-1)
 
-    #initialize the sqlite database
+    # first check for commandline options
+
+    # initialize the sqlite database
     try:
         if os.path.exists(database_path) or os.access(os.path.dirname(database_path), os.W_OK | os.R_OK):
             conf[ConfKeys.DB_CONN] = db.initialize(database_path)
@@ -47,20 +49,20 @@ def main():
     admin.update_eps(conf)
     print "done."
 
-    #couple of usecases:
+    # couple of usecases:
     # 1. there is an argument provided. this is probably a show that the user wants
     #    us to start, so let's start it.
     # 2. there are no arguments provided. provide the user with a query what he wants
 
     if len(args):
-        #1. user provided a showname, find it in the db, then play it.
+        # 1. user provided a showname, find it in the db, then play it.
         s = db.find_show(conf, u' '.join(args))
         if s:
             player.play_next(conf, s)
         else:
             print u'Show "{0}" could not be found!'.format(u' '.join(args))
     else:
-        #2. user provided nothing, popup a list of options
+        # 2. user provided nothing, popup a list of options
         ui = TUI(conf)
         try:
             ui.cmdloop()
