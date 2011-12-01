68a2d497551553b9c2045daa39e0d9ee07d52f46
Peter Wagenaar <sakartu@gmail.com>
2011-12-02 00:59:07 +0100

    built play_show wrapper (fixes issue 29) and refactored a bit

--- next/show/player.py
+++ next/show/player.py
@@ -8,6 +8,18 @@ import re
 import time
 import subprocess
 
+def play_show(conf, show):
+    another = True
+    while another:
+        show = play_next(conf, show)
+        # ask the user for another ep
+        if not show:
+            break
+        print u"Shall I play another ep?"
+        answer = raw_input(u'Another [yes]? ')
+        if u'y' not in answer.lower() and answer.strip() != '':
+            another = False
+
 def play_next(conf, show):
     '''
     This method plays the next episode for the given show.
@@ -18,17 +30,7 @@ def play_next(conf, show):
         print u'Could not find s{S:02d}e{E:02d} for {name}, ep not available or marked maybe_finished?'.format(S=show.season, E=show.ep, name=show.name)
         return
     command = cmd_line.split(' ') + [ep_path]
-    #play the show
-    print u'Starting S{S:02}E{E:02} of {name}!'.format(S=show.season, E=show.ep, name=show.name)
-    try:
-        print " ".join(command)
-        subprocess.call(command)
-    except KeyboardInterrupt:
-        sys.stdout.flush()
-        time.sleep(1) #give the movie player some time to clean up
-    except OSError:
-        # maybe the player isn't installed or something?
-        print u'An error occurred while starting the player, check your config!'
+    if not play(command, show):
         return
 
     #update the db
@@ -39,14 +41,16 @@ def play_next(conf, show):
             next_ep = admin.find_next_ep(conf, show)
             if next_ep:
                 db.change_show(conf, show.sid, next_ep.season, next_ep.epnum)
+                show.season = next_ep.season
+                show.ep = next_ep.epnum
             else:
                 print u'No information about new eps yet, try updating later!'
                 db.mark_maybe_finished(conf, show.sid)
-            print u'Player stopped, database updated.'
-            return
+            return show
     except:
-        print ""
+        print ''
     print u'Database unmodified.'
+    return
 
 def build_ep_path(conf, show):
     '''
@@ -129,3 +133,21 @@ def get_words(text):
     # filter for only normal words (handy in case of "Doctor Who (2005)"
     return filter(lambda x : re.compile(r'^\w*$').match(x), words)
 
+def play(command, show):
+    '''
+    A helper method that executes the given command
+    '''
+    #play the show
+    print u'Starting S{S:02}E{E:02} of {name}!'.format(S=show.season, E=show.ep, name=show.name)
+    try:
+        print " ".join(command)
+        subprocess.call(command)
+        return True
+    except KeyboardInterrupt:
+        sys.stdout.flush()
+        time.sleep(1) #give the movie player some time to clean up
+        return True
+    except OSError:
+        # maybe the player isn't installed or something?
+        print u'An error occurred while starting the player, check your config!'
+        return False
