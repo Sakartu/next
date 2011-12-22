278fc43a5755870fcb81c11f75f534aa18163b96
Peter Wagenaar <sakartu@gmail.com>
2011-12-22 16:49:06 +0100

    added build_sub_path, beautified some comments

--- next/util/util.py
+++ next/util/util.py
@@ -1,8 +1,10 @@
 import subprocess
+import constants
 import textwrap
 import time
 import sys
 import re
+import os
 
 def print_formatted(msg):
     '''
@@ -27,7 +29,7 @@ def play(command, show):
     '''
     A helper method that executes the given command
     '''
-    #play the show
+    # play the show
     print u'Starting S{S:02}E{E:02} of {name}!'.format(S=show.season, E=show.ep, name=show.name)
     try:
         print " ".join(command)
@@ -35,9 +37,23 @@ def play(command, show):
         return True
     except KeyboardInterrupt:
         sys.stdout.flush()
-        time.sleep(1) #give the movie player some time to clean up
+        time.sleep(1) # give the movie player some time to clean up
         return True
     except OSError:
         # maybe the player isn't installed or something?
         print u'An error occurred while starting the player, check your config!'
         return False
+
+def build_sub_path(path):
+    '''
+    A helper function to tell you whether subs are available for a given ep and
+    if so, return the full path to the subtitle file
+    '''
+    if not path:
+        return None
+    (base, _) = os.path.splitext(path)
+    for ext in constants.SUB_EXTS:
+        sub_path = base + '.' + ext
+        if os.path.exists(sub_path):
+            return sub_path
+    return None
