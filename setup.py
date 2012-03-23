eba18c94a3fda3c984ad63da3f2490f0429edda2
Peter Wagenaar <sakartu@gmail.com>
2012-03-23 10:36:43 +0100

    Made next compatible with setuptools

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import os
 
 packages = ['next', 'next.db', 'next.show', 'next.tui', 'next.tvr', 'next.util']
 
-setup(name='next',
-      version='1.1',
-      description="tool to remember which tv serie season and ep you're at and play the next one for you",
-      author='Peter Wagenaar',
-      author_email='sakartu@gmail.com',
-      url='https://github.com/Sakartu/next',
-      packages=packages,
-      scripts=['next.py'],
-      install_requires=[
+setup(name = 'next',
+      version = '1.1',
+      description = "tool to remember which tv serie season and ep you're at and play the next one for you",
+      author = 'Peter Wagenaar',
+      author_email = 'sakartu@gmail.com',
+      url = 'https://github.com/Sakartu/next',
+      packages = packages,
+      keywords = ['show', 'season', 'ep', 'next',],
+      entry_points = '''
+      [console_scripts]
+      next = next.next:main
+      ''',
+      install_requires = [
           'python-tvrage',
           'pyxdg',
           ],
-      test_suite='tests.runtests.runtests',
+      long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
      )
