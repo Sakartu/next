cef0db41a76faa97f4c7492ce58fdf8c289b8fe0
Peter Wagenaar <sakartu@gmail.com>
2012-04-23 09:55:41 +0200

    makes sure that qualities are checked in the right order, even for 0x00 style eps

--- next/util/fs.py
+++ next/util/fs.py
@@ -94,13 +94,14 @@ def build_ep_path(conf, show):
                 continue
 
         if not unstructured:
-            rexes = [re.compile("^" + x.format(show="", season=show.season, ep=show.ep) + ext + "$", re.I | re.U) for
-                    x in constants.SHOW_REGEXES for ext in constants.VIDEO_EXTS] 
+            rexes = [re.compile("^" + x.format(show="", season=show.season,
+                ep=show.ep) + ext + "$", re.I | re.U) for ext in
+                constants.VIDEO_EXTS for x in constants.SHOW_REGEXES]
         else:
             show_words = util.get_words(show.name)
             rexes = [re.compile(x.format(show="".join([word + "[\W_]" for word in
-                show_words]), season=show.season, ep=show.ep) + ext, re.I | re.U) for x in
-                constants.SHOW_REGEXES for ext in constants.VIDEO_EXTS] 
+                show_words]), season=show.season, ep=show.ep) + ext, re.I | re.U)
+                for ext in constants.VIDEO_EXTS for x in constants.SHOW_REGEXES]
         for ep in os.listdir(path):
             for rex in rexes:
                 m = rex.match(ep)
