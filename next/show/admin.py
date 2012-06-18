84fb2414ebc193d369f13c3b258919ec20c5b9be
Peter Wagenaar <sakartu@gmail.com>
2012-06-18 16:11:49 +0200

    add a couple of retries when updating the tvr db

--- next/show/admin.py
+++ next/show/admin.py
@@ -1,5 +1,6 @@
 from db import db
 from tvr import parser
+from urllib2 import URLError, HTTPError
 import util.util as util
 import os
 
@@ -9,16 +10,17 @@ def find_unlisted(conf):
     This method searches in your shows folder for shows that aren't in the
     database yet
     '''
-    listed = map(lambda x : x.name, db.all_shows(conf))
+    listed = map(lambda x: x.name, db.all_shows(conf))
     basedir = os.path.expanduser(conf['show_path'])
     try:
-        all_shows = [ d for d in os.listdir(basedir) if os.path.isdir(os.path.join(basedir, d)) ]
+        all_shows = [d for d in os.listdir(basedir) if os.path.isdir(
+            os.path.join(basedir, d))]
     except OSError:
         print "Could not list directory {0}".format(basedir)
         all_shows = []
 
     has_match = lambda s: any(x for x in listed if util.shows_match(s, x))
-    return [ s for s in all_shows if not has_match(s) ]
+    return [s for s in all_shows if not has_match(s)]
 
 
 def find_next_ep(conf, show):
@@ -28,7 +30,7 @@ def find_next_ep(conf, show):
     next_season = show.season
     next_ep = show.ep + 1
     ep = db.find_ep(conf, show.sid, next_season, next_ep)
-    if ep == None: # maybe forward to next season?
+    if ep == None:  # maybe forward to next season?
         next_season = show.season + 1
         next_ep = 1
         ep = db.find_ep(conf, show.sid, next_season, next_ep)
@@ -47,19 +49,39 @@ def update_eps(conf, output=True):
         for show in all_shows:
             if output:
                 print '.',
-            status = parser.get_status(show.sid)
-            all_eps = parser.get_all_eps(show.sid)
+
+            all_eps = []
+            for i in range(4):  # try updating 4 times, tvrdb is a bit flakey
+                try:
+                    status = parser.get_status(show.sid)
+                    all_eps = parser.get_all_eps(show.sid)
+                    break
+                except URLError:
+                    continue
+
+            if not all_eps:
+                raise URLError
+
             db.change_status(conf, show.sid, status)
             db.store_tvr_eps(conf, all_eps)
-    except Exception, e: # probably no internet connection
-        print "\nCould not connect to TVRage, will not update tvrage episode cache:"
-        print e
+    except URLError, e:  # probably no internet connection
+        print '\nCould not connect to TVRage, will not update tvrage episode'
+        ' cache:'
+        print e.reason
+        return
+    except Exception, e:  # probably no internet connection
+        print '\nCould not connect to TVRage, will not update tvrage episode'
+        ' cache.'
+        if hasattr(e, 'code'):
+            print 'Received status code:'
+            print e.code
         return
 
     try:
         process_maybe_finished(conf, all_shows)
     except:
-        print u'maybe_finished processing failed for tvrage episode cache update, database may be in an inconsistant state!'
+        print u'maybe_finished processing failed for tvrage episode cache '
+        'update, database may be in an inconsistant state!'
         raise Exception
 
     if output:
@@ -68,10 +90,10 @@ def update_eps(conf, output=True):
 
 def process_maybe_finished(conf, all_shows):
     '''
-    This method walks over all the shows that are marked maybe_finished, to find
-    out if they're really finished or if the database was just outdated. If a
-    new ep can be found for the given show, this is set as the current ep and
-    the maybe_finished mark is removed
+    This method walks over all the shows that are marked maybe_finished, to
+    find out if they're really finished or if the database was just outdated.
+    If a new ep can be found for the given show, this is set as the current ep
+    and the maybe_finished mark is removed
     '''
     for show in all_shows:
         if show.maybe_finished:
